72b4b2393788680c9e6c49215aa297c73d2bf58c
thngndangbun <108016100+thngndangbun@users.noreply.github.com>
2022-06-22 23:20:53 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,20 +1,20 @@
 const CONFIG = {
     titleWeb: " Lưu Bút năm 12 ",
-    introTitle: 'Tên của bạn',
+    introTitle: 'Thanh Ngân',
     introDesc: `Trái đất vốn lạ thường
     Mà sao em cứ đi nhầm đường
     Lạc vào tim anh lẻ loi
     Đằng sau chữ yêu đây là thương`,
     btnIntro: '^^HiHi^^',
     title: 'Phải chăng định mệnh đã đưa ta gặp nhau để rồi làm bạn và chơi dới nhau như thế này!! 🥰',
-    desc: 'Nói v chứ thí ghéc lắm nhaa ',
+    desc: 'Phải vậy hong taaa? ',
     btnYes: 'Thích lắm <33',
     btnNo: 'Không nha :3',
     question: 'Trên thế giới hơn 7 tỉ người mà sao chúng ta lại gặp và nói chuyện dới nhau được nhờ, duyên phận cả đấy <3',
     btnReply: 'Gửi cho bạn <3',
-    reply: 'Cám Ơn Bạn Vì Đã Là 1 phần thanh xuân của mình, hãy hạnh phúc với con đường phía trước nhé',
-    mess: 'Mình biết bạn cũng quí mình mà',
+    reply: '',
+    mess: 'Mình biết bạn cũng quí mình mà, cám Ơn Bạn Vì Đã Là 1 phần thanh xuân của mình, hãy hạnh phúc với con đường phía trước nhé',
     messDesc: 'Cùng nhao hạnh phúc nhé',
     btnAccept: ' <3',
-    messLink: 'https://www.facebook.com/thhhngann ,Hãy giữ liên lạc với mình nhé'
+    messLink: 'https://www.facebook.com/thhhngann,'
 }
